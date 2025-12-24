b80692139f722acfb188d2d72240ea6c10e9306e
Gali Bhaskar <luciferbhaskar@gmail.com>
2025-12-23 19:08:31 -0700

    📝 Add Pseudo Code for Q4
    Add Pseudo Code for Q4

---
+++ Q4_Solution.java
@@ -0,0 +1,27 @@
+/* 
+    Q4.  Write pseudocode to check if a string is a palindrome.
+
+    Input: "racecar" → true
+    Input: "hello" → false
+*/
+
+public class Q4_Solution {
+   /*
+
+        Pseudocode:
+
+            Two pointer approach
+
+            1. Use two pointers, one at the start and one at the end of the string.
+            2. Compare the characters at the two pointers.
+            3. If they are not the same, return false.
+            4. If they are the same, move the pointers towards the center(start++, end--)
+            5. If the pointers meet in the middle, return true.
+
+        Time Complexity: O(n)
+            n: length of the string
+
+        Space Complexity: O(1)
+            we are using two variables which is constant space.
+    */
+}
